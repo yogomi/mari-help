5aedb411c63828d263deb200d78980b0664b642e
Makoto Yano <yan133@gmail.com>
2022-02-25 15:41:56 +0900

    改善案

--- 2022-feb.js
+++ 2022-feb.js
@@ -1,3 +1,27 @@
+function dateStringToJPDate(dateString) {
+  var monthTable = {
+    Jan: 1, Feb: 2, Mar: 3, Apr: 4, May: 5, Jun: 6,
+    Jul: 7, Aug: 8, Sep: 9, Oct: 10, Nov: 11, Dec: 12
+  };
+
+  var weekTable = {
+    Sun: '日', Mon: '月', Tue: '火', Wed: '水', Thu: '木', Fri: '金', Sat: '土'
+  }
+
+  var dateObj = {};
+  [
+    dateObj.dateOfWeek,
+    dateObj.month,
+    dateObj.date,
+    dateObj.year,
+    dateObj.time,
+    dateObj.locale,
+    dateObj.timeDelta,
+    dateObj.timeDeltaString
+  ] = dateString.split(' ');
+   return `${dateObj.year}年${monthTable[dateObj.month]}月${dateObj.date}日（${weekTable[dateObj.dateOfWeek]}）`;
+}
+
 function checkUpdate(){
   var mySheet = SpreadsheetApp.getActiveSheet(); //シートを取得
   var myCell = mySheet.getActiveCell(); //アクティブセルを取得
@@ -9,7 +33,7 @@ function checkUpdate(){
   var q4 = mySheet.getRange(row, 33).getValue();
   var q5 = mySheet.getRange(row, 28).getValue();
   var q6 = mySheet.getRange(row, 27).getValue();
-  var message = "\n■月日:" + q0 + "\n■氏名:" + q1 + "\n■人数:" + q2 + "人\n■所在:" + q3 + "\n■Name:" + q4 + "\n■Number:" + q6 + "\n■Country:" + q5;
+  var message = "\n■月日:" + dateStringToJPDate(q0) + "\n■氏名:" + q1 + "\n■人数:" + q2 + "人\n■所在:" + q3 + "\n■Name:" + q4 + "\n■Number:" + q6 + "\n■Country:" + q5;
   sendLine(message);
 }
 
